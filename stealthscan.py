9218b14838b92b8fc2405e5b9509b885b0c5ed9c
enki-- <pbvinge@gmail.com>
2018-03-17 08:25:04 -1000

    trying to fix 2nd rst

--- stealthscan.py
+++ stealthscan.py
@@ -4,12 +4,18 @@
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
+import subprocess
 from scapy.all import sr, sr1, IP, TCP, RandShort
 
 dst_ip = "216.58.219.36"
 src_port = RandShort()
 dst_port=80
 set_ttl=128
+
+
+p = subprocess.Popen(["iptables", "-A", "OUTPUT", "-p", "tcp", "--tcp-flags", "RST", "RST", -s dst_ip, "-j" "DROP"], stdout=subprocess.PIPE)
+        output , err = p.communicate()
+        print output
 
 stealth_scan_resp = sr1(IP(dst=dst_ip,ttl=set_ttl)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=2)
 if(str(type(stealth_scan_resp))=="<type 'NoneType'>"):
@@ -23,3 +29,9 @@ elif (stealth_scan_resp.getlayer(TCP).flags == 0x14):
 elif(stealth_scan_resp.haslayer(ICMP)):
     if(int(stealth_scan_resp.getlayer(ICMP).type)==3 and int(stealth_scan_resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
         print("Filtered")
+
+
+
+p = subprocess.Popen(["iptables", "-D", "OUTPUT", "-p", "tcp", "--tcp-flags", "RST", "RST", -s dst_ip, "-j" "DROP"], stdout=subprocess.PIPE)
+        output , err = p.communicate()
+        print output
