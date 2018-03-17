dfff949a01b1b76d3a9ca3760724c2c3f8191ee9
enki-- <pbvinge@gmail.com>
2018-03-17 14:46:56 -0400

    3 packets!

--- stealthscan.py
+++ stealthscan.py
@@ -8,21 +8,21 @@ import subprocess
 from scapy.all import sr, sr1, IP, TCP, RandShort
 
 dst_ip = "216.58.219.36"
-src_port = RandShort()
+src_port = 2525
 dst_port=80
 set_ttl=128
 
 
-p = subprocess.Popen(["iptables", "-A", "OUTPUT", "-p", "tcp", "--tcp-flags", "RST", "RST", "-s" dst_ip, "-j" "DROP"], stdout=subprocess.PIPE)
-        output , err = p.communicate()
-        print output
+p = subprocess.Popen(["iptables", "-A", "OUTPUT", "-p", "tcp", "--tcp-flags", "RST", "RST", "-d", dst_ip, "-j" "DROP"], stdout=subprocess.PIPE)
+output , err = p.communicate()
+print(output)
 
 stealth_scan_resp = sr1(IP(dst=dst_ip,ttl=set_ttl)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=2)
 if(str(type(stealth_scan_resp))=="<type 'NoneType'>"):
     print("Filtered")
 elif(stealth_scan_resp.haslayer(TCP)):
-#    if(stealth_scan_resp.getlayer(TCP).flags == 0x12):
-#        send_rst = sr(IP(dst=dst_ip,ttl=set_ttl)/TCP(sport=src_port,dport=dst_port,flags="R"),timeout=2)
+    if(stealth_scan_resp.getlayer(TCP).flags == 0x12):
+        send_rst = sr(IP(dst=dst_ip,ttl=set_ttl)/TCP(sport=src_port,dport=dst_port,flags="R"),timeout=2)
     print("Open")
 elif (stealth_scan_resp.getlayer(TCP).flags == 0x14):
     print("Closed")
@@ -32,6 +32,7 @@ elif(stealth_scan_resp.haslayer(ICMP)):
 
 
 
-p = subprocess.Popen(["iptables", "-D", "OUTPUT", "-p", "tcp", "--tcp-flags", "RST", "RST", "-s" dst_ip, "-j" "DROP"], stdout=subprocess.PIPE)
-        output , err = p.communicate()
-        print output
+p = subprocess.Popen(["iptables", "-D", "OUTPUT", "-p", "tcp", "--tcp-flags", "RST", "RST", "-d", dst_ip, "-j" "DROP"], stdout=subprocess.PIPE)
+
+output , err = p.communicate()
+print(output)
